963f02dc06d37ea24ad3779dd8d73796f5891bdc
Ubuntu <ubuntu@ip-172-31-12-191.us-east-2.compute.internal>
2025-03-20 02:24:56 +0000

    First commit of the project

---
+++ todo/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateTimeField(null=True, blank=True)
+    is_done = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
